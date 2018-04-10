9fa1953d705c092e69925ad9e49fdea5cf938794
LiuJoho <18065934070@sina.cn>
2018-04-10 23:50:23 +0800

    4.10

--- datageting/src/com/ys/entity/Village.java
+++ datageting/src/com/ys/entity/Village.java
@@ -2,4 +2,56 @@ package com.ys.entity;
 
 public class Village {
 
+	private String village;
+	private String longitude;
+	private String latitude;
+	private String location;
+	private String county;
+	
+	public String getVillage() {
+		return village;
+	}
+	
+	public Village(String village, String location, String county) {
+		super();
+		this.village = village;
+		this.location = location;
+		this.county = county;
+	}
+
+	public void setVillage(String village) {
+		this.village = village;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCounty() {
+		return county;
+	}
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	
+	@Override
+	public String toString() {
+		return "Village [village=" + village + ", longitude=" + longitude
+				+ ", latitude=" + latitude + ", location=" + location
+				+ ", county=" + county + "]";
+	}
+		
 }
--- datageting/src/getdata/JspGet2.java
+++ datageting/src/getdata/JspGet2.java
@@ -4,17 +4,24 @@ package getdata;
 import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+
+import com.ys.entity.Village;
 
 import util.DBUtil;
 import util.HttpClientUtil;
 
 
 public class JspGet2 {
-public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException {
 		MyThreadRule4 tr = new MyThreadRule4();
-		tr.start();
+		tr.start();		
 	}
 
 	
@@ -23,20 +30,28 @@ public static void main(String[] args) throws FileNotFoundException {
 class MyThreadRule4 extends Thread{
 	@Override
 	public void run() {
-		
-		
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("思明区","350203");
+		map.put("海沧区","350205");
+		map.put("湖里区","350206");
+		map.put("集美区","350211");
+		map.put("同安区","350212");
+		map.put("翔安区","350213");
+		List<Village> list = new ArrayList<Village>();
+		list = queryInfo();
 		String url = "";
 		//String[] quyu = {"350203","350205","350206","350211","350212","350213"};
-		String[] quyu = {"350203"};
-		for (int i = 0; i < quyu.length; i++) {	
+		for (Village village : list) {
 			int num = 0;
 			boolean max = true;
 			while(max){
-				url = "http://zawb.fjgat.gov.cn/weixin/zhfw/czw_qwjs_cx.jsp?sunitname=前埔"
-						+ "&phrase=福建省厦门市"
+				url = "http://zawb.fjgat.gov.cn/weixin/zhfw/czw_qwjs_cx.jsp?sunitname="+village.getLocation()
+						+ "&phrase=福建省厦门市"+village.getCounty()
 						+ "&rowpage="+num
-						+"&ss_qx="+quyu[i]+"&rPageSize=15";
+						+"&ss_qx="+map.get(village.getCounty())+"&rPageSize=15";
+				System.out.println("url:" + url);
 				String a = HttpClientUtil.doGet(url);
+				System.out.println(a);
 				if (a.indexOf("<li") != -1) {
 					num++;
 					String[] b = a.split("</li>");		
@@ -45,18 +60,77 @@ class MyThreadRule4 extends Thread{
 						String d = c.substring(c.indexOf(",") + 1, c.length()).replace("'", "");
 						String urlPath = "http://www.fjadd.com/addr?id=" + d;
 						System.out.println(urlPath);
-						getInfo(urlPath);
+						getInfo(urlPath,village.getVillage());
 					}
 				}else{
 					max = false;
+					if (num == 0) {
+						System.out.println(village.getVillage() + "," 
+					+ village.getLocation() + ","+ village.getCounty());
+					}
 				}
-			}
-					
+			}								
 		}						
 	
 	}
 	
-	public void getInfo(String str){
+	public static List<Village> queryInfo(){
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<Village> list = null;
+		try{
+			conn = DBUtil.getConn();			
+			String sql = "select * from village_report";
+			ps = conn.prepareStatement(sql);
+			rs= ps.executeQuery();
+			list = new ArrayList<Village>();
+			while (rs.next()){
+	            list.add(new Village(rs.getString("village"),rs.getString("location"),rs.getString("county")));
+	        }
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			 DBUtil.closeConn(conn);
+	            if (null != ps) {
+	                try {
+	                    ps.close();
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	               }
+	           }
+	         
+		}
+		return list;
+	}
+	
+	public void insertTemp(String village,String location,String county){
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try{
+			conn = DBUtil.getConn();			
+				String sql = "insert into village_temp (village,location,county)values(?,?,?)";
+				ps = conn.prepareStatement(sql);
+				ps.setString(1, village);
+				ps.setString(2, location);
+				ps.setString(3, county);
+				ps.executeUpdate();
+				//System.out.println(Thread.currentThread().getName());
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			 DBUtil.closeConn(conn);
+	            if (null != ps) {
+	                try {
+	                    ps.close();
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	               }
+	           }
+		}
+	}
+	
+	public void getInfo(String str,String village){
 		Connection conn = null;
 		PreparedStatement ps = null;
 		try{
@@ -76,16 +150,16 @@ class MyThreadRule4 extends Thread{
 					int imgTo = imgStr.indexOf("')");
 					imgUrl = imgStr.substring(9, imgTo);
 				}
-				String  bb = a.substring(i+6,j-1);
+				String[] bb = a.substring(i+6,j-1).split(",");
 				String addr = a.substring(c+5,d-1);
-				String sql = "insert into mobile_add (id,longitude_latitude,address,picurl,pic,police,village)values(?,?,?,?,?,?,?)";
+				String sql = "insert into village_add (id,latitude,longitude,address,picurl,police,village)values(?,?,?,?,?,?,?)";
 				ps = conn.prepareStatement(sql);
 				ps.setString(1, id.toString());
-				ps.setString(2, bb);
-				ps.setString(3, addr);
-				ps.setString(4, imgUrl);
+				ps.setString(2, bb[0]);
+				ps.setString(3, bb[1]);
+				ps.setString(4, addr);
 				int policeInt = a.indexOf("line-height: 20px;\">");
-				ps.setBinaryStream(5, null);
+				ps.setString(5, imgUrl);
 				if(policeInt>0){
 					String policeStr = a.substring(policeInt+1);
 					String police = policeStr.substring(0, policeStr.indexOf("</a>"));
@@ -98,10 +172,10 @@ class MyThreadRule4 extends Thread{
 				}else{
 					ps.setString(6, null);
 				}
-				ps.setString(7, "前埔村");
+				ps.setString(7,village);
 				ps.execute();
 				System.out.println(Thread.currentThread().getName());
-				System.out.println("【获得的经纬度："+bb+"】,【地址："+addr+"】,【图片地址:"+imgUrl+"】");
+				System.out.println("【获得的纬度："+bb[0]+"】,【地址："+addr+"】,【图片地址:"+village+"】"+"】,【获得的经度："+bb[1]+"】");
 			}
 		}catch(Exception e){
 			e.printStackTrace();
